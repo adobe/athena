5e9989ea0015f1426646bc068b5bdf8d9b83f843
trifan <trifan@adobe.com>
2019-07-25 05:50:32 +0300

    power

--- atena.js
+++ atena.js
@@ -1,4 +1,23 @@
-const expect = require("chakram");
-describe = function() {
+describe = function(title, fn) {
+    let result = fn();
+};
+
+afterEach = function(fn) {
 
 };
+
+const chakram = require('chakram')
+
+const expect = chakram.expect;
+
+chakram.addProperty("spotify", function(){});
+chakram.addMethod("error", function (respObj, status, message) {
+    expect(respObj).to.have.schema(spotifyErrorSchema);
+    expect(respObj).to.have.status(status);
+    expect(respObj).to.have.json('error.message', message);
+    expect(respObj).to.have.json('error.status', status);
+});
+
+describe('hope', function() {
+   expect(true).to.be(true);
+});
