af1ca4e215e86f87df18836cb538e6f74c14ccff
trifan <trifan@adobe.com>
2019-07-26 03:08:49 +0300

    made it run

--- atena.js
+++ atena.js
@@ -1,23 +1,51 @@
-describe = function(title, fn) {
-    let result = fn();
+
+var pathModule = require('path');
+var assert = require('assert').ok;
+
+const registerdSuites = {};
+
+module.constructor.prototype.require = function (path) {
+
+    if(pathModule.extname(path) == ".atena") {
+        return registerdSuites[pathModule.basename(path)]();
+    }
+
+    var self = this;
+    assert(typeof path === 'string', 'path must be a string');
+    assert(path, 'missing path');
+
+    try {
+        return self.constructor._load(path, self);
+    } catch (err) {
+        // if module not found, we have nothing to do, simply throw it back.
+        if (err.code === 'MODULE_NOT_FOUND') {
+            throw err;
+        }
+        // resolve the path to get absolute path
+        path = pathModule.resolve(__dirname, path)
+
+        // Write to log or whatever
+        console.log('Error in file: ' + path);
+    }
+}
+const Mocha = require('mocha');
+const mocha = new Mocha();
+
+registerdSuites["testing.atena"] = function() {
+    const chakram = require('chakram');
+    const expect = chakram.expect;
+    chakram.addProperty("true", function(object){
+        assert(object, true);
+    });
+
+    describe('hope', function() {
+        it('shodld be ok', function() {
+            return expect(true).to.be.true;
+        })
+    });
 };
 
-afterEach = function(fn) {
-
-};
-
-const chakram = require('chakram')
-
-const expect = chakram.expect;
-
-chakram.addProperty("spotify", function(){});
-chakram.addMethod("error", function (respObj, status, message) {
-    expect(respObj).to.have.schema(spotifyErrorSchema);
-    expect(respObj).to.have.status(status);
-    expect(respObj).to.have.json('error.message', message);
-    expect(respObj).to.have.json('error.status', status);
-});
-
-describe('hope', function() {
-   expect(true).to.be(true);
+mocha.addFile("testing.atena");
+mocha.run(function(failures) {
+    process.exitCode = failures ? 1 : 0;  // exit with non-zero status if there were failures
 });
