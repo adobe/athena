b6640430ec36659b1c109dc8e11043b96ebba23a
Catalin Vasile <nvasile@adobe.com>
2019-10-16 15:51:26 +0300

    Add license headers to source files

--- src/engines/autocannonEngine.js
+++ src/engines/autocannonEngine.js
@@ -1,14 +1,20 @@
-const cluster = require("cluster"),
-    numCPUs = require("os").cpus().length,
-    pid = process.pid;
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
 const autocannon = require('autocannon');
 
 const Engine = require("./engine"),
     {TAXONOMIES, ENGINES} = require("./../enums"),
-    {makeLogger, isPerformanceTest, isPerformancePattern, removeEmpty} = require("./../utils");
-
-const log = makeLogger();
+    {isPerformanceTest, isPerformancePattern, removeEmpty} = require("./../utils");
 
 class AutocannonEngine extends Engine {
     constructor(settings, entityManager, pluginManager) {
--- src/managers/pluginsManager.js
+++ src/managers/pluginsManager.js
@@ -1,20 +1,31 @@
-// node
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+// Node
 const path = require("path"),
     fs = require("fs"),
     _require = require("child_process"),
     execSync = _require.execSync;
 
-// external
+// External
 const {isArray, pullAll, uniq} = require("lodash"),
     detective = require("detective");
 
-// project
+// Project
 const {
     makeLogger,
     makeContainer,
     startSpinner,
-    getPackageInstallCommand,
-    isTest,
+    getPackageInstallCommand
 } = require("../utils");
 
 // todo: load dynamically
