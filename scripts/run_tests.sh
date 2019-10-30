9ad02c199e2d5fce9e81c611461d7ea245c66fe3
trifan <trifan@adobe.com>
2019-10-30 17:43:46 +0200

    power commit

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 envoy -c /etc/envoy/config/envoy.yaml -l info --concurrency 8 &
-./src/sidecar/k8/scripts/ip-tables-hook.sh
+sudo ./src/sidecar/k8/scripts/ip-tables-hook.sh
 node ./athena.js run --performance -t /etc/tests
--- src/sidecar/k8/scripts/ip-tables-hook.sh
+++ src/sidecar/k8/scripts/ip-tables-hook.sh
@@ -2,16 +2,16 @@
 sysctl -w net.ipv4.conf.eth0.route_localnet=1
 sysctl -w net.ipv4.ip_forward=1
 
-function prerouting {
-    iptables -t nat -A PREROUTING -p tcp --dport 80:3999 -j REDIRECT --to-port 8080
-    iptables -t nat -A PREROUTING -p tcp --dport 4001:65535 -j REDIRECT --to-port 8080
+prerouting() {
+    iptables -t nat -A PREROUTING -p tcp --dport 80:3999 -j REDIRECT --to-port 9191
+    iptables -t nat -A PREROUTING -p tcp --dport 4001:65535 -j REDIRECT --to-port 9191
     iptables -A INPUT -j ACCEPT
     iptables -A FORWARD -j ACCEPT
 }
 
-function postrouting {
-    iptables --match multiport -t nat -A OUTPUT -p tcp --dports 80,443 -j DNAT --to-destination 127.0.0.1:9191
+postrouting() {
+    sudo iptables --match multiport -t nat -A OUTPUT -p tcp -m owner ! --uid-owner sidecar --dports 80,443 -j DNAT --to-destination 127.0.0.1:9191
 }
 
 postrouting
-iptables-save
+sudo iptables-save
